ceb92213b3a0451c19da46a41c138a1103a9b59a
abhimanyuvigyani <abhimanyuvigyani@gmail.com>
2026-08-21 13:27:19 +0530

    Create Question_8.cs

---
+++ Question_8.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class University
+{
+    public class Department
+    {
+        public string DepartmentName;
+        public string HODName;
+        public int FacultyMembers;
+
+        public Department(string departmentName, string hodName, int facultyMembers)
+        {
+            DepartmentName = departmentName;
+            HODName = hodName;
+            FacultyMembers = facultyMembers;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Department: " + DepartmentName);
+            Console.WriteLine("HOD: " + HODName);
+            Console.WriteLine("Faculty Members: " + FacultyMembers);
+        }
+    }
+}
+
+class Que_8
+{
+    static void Main()
+    {
+        University.Department d = new University.Department(
+            "Computer Science",
+            "Rohit Agrawal",
+            25
+        );
+
+        d.Display();
+    }
+}
