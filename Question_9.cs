663b333a0790b62c8b3a9d2df1736a28bf63330c
abhimanyuvigyani <abhimanyuvigyani@gmail.com>
2026-08-21 13:28:00 +0530

    Create Question_9.cs

---
+++ Question_9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+sealed class AppConfig
+{
+    public static string ConnectionString = "Server=localhost;Database=CollegeDB;";
+    public static string AppName = "GLA FriendsBook";
+    public static string Version = "1.0";
+
+    private AppConfig()
+    {
+    }
+
+    public static void Display()
+    {
+        Console.WriteLine("App Name: " + AppName);
+        Console.WriteLine("Version: " + Version);
+        Console.WriteLine("Connection: " + ConnectionString);
+    }
+}
+
+class Que_9
+{
+    static void Main()
+    {
+        AppConfig.Display();
+    }
+}
