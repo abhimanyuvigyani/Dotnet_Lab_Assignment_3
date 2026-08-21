5aa95199ccdac9a6b0c1d16a458843f983d0e994
abhimanyuvigyani <abhimanyuvigyani@gmail.com>
2026-08-21 13:14:22 +0530

    Create Question_3.cs

---
+++ Question_3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ShapeApplication
+{
+    abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Length;
+        public double Width;
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return Length * Width;
+        }
+    }
+
+    class Que_3
+    {
+        static void Main() 
+        {
+            Circle circle = new Circle(5);
+            Rectangle rectangle = new Rectangle(10, 5);
+
+            Console.WriteLine("Circle Area: " + circle.CalculateArea());
+            Console.WriteLine("Rectangle Area: " + rectangle.CalculateArea());
+        }
+    }
+}
